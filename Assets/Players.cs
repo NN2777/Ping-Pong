9d7be25a952409df94c7e4894154125bc5c5d26c
NN2777 <naufanafidn@gmail.com>
2022-06-09 16:27:25 +0700

    Base gameplay

---
+++ Assets/Players.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float movement;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlayer1){
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+        rb.velocity = new Vector2(rb.velocity.x, movement*speed );
+    }
+}
